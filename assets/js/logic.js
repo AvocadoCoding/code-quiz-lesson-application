7149f9b058c0d6fad4e7185d85d323fdb71de7d8
AvocadoCoding <116954089+AvocadoCoding@users.noreply.github.com>
2022-12-09 19:52:11 +0000

    Event listener added to start button

---
+++ assets/js/logic.js
@@ -0,0 +1,22 @@
+// Add an event button to start quiz- goes to first question and starts timer
+var startButton = document.querySelector("#todo-text");
+startButton.addEventListener = ('click',function(){
+    firstQuestion();
+    startTimer;
+})
+
+function firstQuestion(){
+
+}
+
+function startTimer(){
+
+}
+
+// Add an apendchild function to render question and list of answers, each answer
+// must be a button
+
+
+
+
+// When quick is completed, change window location to open the 'highscores.html' file
