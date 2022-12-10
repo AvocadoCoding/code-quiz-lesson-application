094f8f14328f0718d11e7fa102537df4bd4c6933
AvocadoCoding <116954089+AvocadoCoding@users.noreply.github.com>
2022-12-10 00:45:43 +0000

    function to hide quiz start elements added

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,17 +1,42 @@
+//Remove all local storage items:
+// localStorage.clear();
+
+/* window.addEventListener("load",{
+    console.log("page is fully loaded");
+});
+ */
+
 // Add an event button to start quiz- goes to first question and starts timer
-var startButton = document.querySelector("#todo-text");
-startButton.addEventListener = ('click',function(){
-    firstQuestion();
-    startTimer;
-})
+var startButton = document.querySelector("#start");
+var startScreen = document.querySelector("#start-screen");
+var timeCounter = document.querySelector("#time");
+var questions = document.querySelector("#questions");
+var questionTitle = document.querySelector("#question-title");
 
 function firstQuestion(){
-
+// Remove existing elements in main section of page
+startScreen.setAttribute("style","display:none;");
+    
+    
+// Add new elements to page
+questions.setAttribute("style","display:inline;");
+questionTitle.textContent = "Test";       
 }
-
+    
 function startTimer(){
-
+    timeCounter.textContent="75";
 }
+
+function btnTrigger(){
+    console.log("test");
+    firstQuestion();
+    // startTimer(); 
+}
+
+
+startButton.addEventListener = ("click", btnTrigger());
+
+
 
 // Add an apendchild function to render question and list of answers, each answer
 // must be a button
@@ -19,4 +44,4 @@ function startTimer(){
 
 
 
-// When quick is completed, change window location to open the 'highscores.html' file
+// When question is completed, change window location to open the 'highscores.html' file
